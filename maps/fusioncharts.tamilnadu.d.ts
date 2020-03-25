e346796fa1f20f93b00ff0000e8216eda8eef2bb
Venkatraman Muthukrishnan <madurai.mvr@gmail.com>
2020-03-25 11:32:43 +0530

    inital commit

---
+++ maps/fusioncharts.tamilnadu.d.ts
@@ -0,0 +1,8 @@
+
+import { FusionChartStatic } from 'fusioncharts';
+
+declare namespace Tamilnadu {}
+declare var Usa: (H: FusionChartStatic) => FusionChartStatic;
+export = Tamilnadu;
+export as namespace Tamilnadu;
+
